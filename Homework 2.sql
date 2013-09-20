e399f883826a7e3a39a0c2abf2ebb8621e6b50f4
Jaimobean <Jaimobean@aol.com>
2013-09-20 16:12:42 -0400

    First answer to HW 2

--- Homework 2.sql
+++ Homework 2.sql
@@ -3,3 +3,11 @@
 --Due 9/23
 
 --Question 1
+--Selects the cities of agents booking an order for customer c002 ('Basics')
+select city
+from agents
+where aid in (select aid 
+			  from orders
+			  where cid = (select cid
+						   from customers
+						   where name = 'Basics'))
