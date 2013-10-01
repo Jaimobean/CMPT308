9d124295d3d3b19806a16df1ef5abd7673ea23f2
Jaimobean <Jaimobean@aol.com>
2013-10-01 14:36:46 -0400

    Answer to number 2 on hw3

--- Homework 3.sql
+++ Homework 3.sql
@@ -14,13 +14,18 @@ where aid in (select aid
 --Question 2
 --Selects the cities of agents booking an order for customer c002 (joins)
 
-
-
+select a.city
+from agents a, 
+     orders o,
+     customers c
+where o.aid = a.aid
+  and o.cid = c.cid
+  and c.cid = 'c002'
 
 
 --Question 3
 --Selects pids of products ordered though any agent who makes at least one 
---order for a customer in Kyoto
+--order for a customer in Kyoto (subqueries)
 
 select distinct pid
 from orders
@@ -30,4 +35,10 @@ where aid in(select aid
 			  from customers
 			  where city = 'Kyoto'))
 order by pid;
+
+--Question 4
+--Selects pids of products ordered though any agent who makes at least one 
+--order for a customer in Kyoto (joins)
+
+
 
